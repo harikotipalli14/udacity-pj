23b5868bf885648ad6c173e3271b56068410ff01
harikotipalli14 <harikotipalli14@gmail.com>
2026-09-22 20:00:37 +0530

    Add CI/CD workflows and modernize EKS setup

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -149,7 +149,29 @@ resource "aws_eks_cluster" "main" {
     endpoint_public_access  = var.enable_private == true ? false : true
     endpoint_private_access = true
   }
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
   depends_on = [aws_iam_role_policy_attachment.eks_cluster, aws_iam_role_policy_attachment.eks_service]
+}
+
+resource "aws_eks_access_entry" "github_action_user" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = aws_iam_user.github_action_user.arn
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "github_action_user" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = aws_iam_user.github_action_user.arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+
+  depends_on = [aws_eks_access_entry.github_action_user]
 }
 
 
@@ -188,7 +210,7 @@ resource "aws_iam_role_policy_attachment" "eks_service" {
 ##################
 # Track latest release for the given k8s version
 data "aws_ssm_parameter" "eks_ami_release_version" {
-  name = "/aws/service/eks/optimized-ami/${aws_eks_cluster.main.version}/amazon-linux-2/recommended/release_version"
+  name = "/aws/service/eks/optimized-ami/${aws_eks_cluster.main.version}/amazon-linux-2023/x86_64/standard/recommended/release_version"
 }
 
 resource "aws_eks_node_group" "main" {
@@ -197,6 +219,7 @@ resource "aws_eks_node_group" "main" {
   version         = aws_eks_cluster.main.version
   node_role_arn   = aws_iam_role.node_group.arn
   subnet_ids      = [var.enable_private == true ? aws_subnet.private_subnet.id : aws_subnet.public_subnet.id]
+  ami_type        = "AL2023_x86_64_STANDARD"
   release_version = nonsensitive(data.aws_ssm_parameter.eks_ami_release_version.value)
   instance_types  = ["t3.small"]
 
